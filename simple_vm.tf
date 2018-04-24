57b0a6eb750b81fa41835f0dae5997671b07c830
Matsca09 <Matsca09@users.noreply.github.com>
2018-04-24 18:05:05 +0200

    Update VM TF example with modules and remove VM+Network TF example

--- simple_vm.tf
+++ simple_vm.tf
@@ -1,16 +1,28 @@
-### Simple Ubuntu 16.04 VM
+# Example VM and security group defined by using EnterCloudSuite terraform modules
+# https://github.com/entercloudsuite/terraform-modules
 
-resource "openstack_compute_instance_v2" "simple-vm" {
-  name            = "simple-tf-vm"
-  region          = "${var.region}"
-  count           = 1
-  image_id        = "6a1e4c2b-d663-492a-a828-205f4b28d9e0"
-  flavor_name     = "e3standard.x2"
-  key_pair        = "${openstack_compute_keypair_v2.keypair.name}"
-  security_groups = ["default"]
+# Create ssh firewall policy
+module "demo-instance-ssh-sg" {
+  source = "github.com/entercloudsuite/terraform-modules//security?ref=2.6"
+  name = "demo-instance-ssh-sg"
+  region = "${var.region}"
+  protocol = "tcp"
+  port_range_min = 22
+  port_range_max = 22
+  allow_remote = "0.0.0.0/0"
+}
 
-  network {
-    uuid = "${openstack_networking_network_v2.production-internal-network.id}"
+# Create instance
+module "demo-instance" {
+  source = "github.com/entercloudsuite/terraform-modules//instance?ref=2.6"
+  name = "demo-instance"
+  quantity = 1
+  external = "true"
+  flavor = "e3standard.x1"
+  network_name = "${var.network_name}"
+  sec_group = ["${module.demo-instance-ssh-sg.sg_id}"]
+  keypair = "${var.keypair_name}"
+  tags = {
+    "server_group" = "DEMO"
   }
-
 }
